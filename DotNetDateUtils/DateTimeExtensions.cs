e6bd20bc94720c843475f36b0f62657236486045
Ruaan Viljoen <ruaanv@gmail.com>
2019-01-21 11:32:27 +0200

    Add better descriptions for methods

--- DotNetDateUtils/DateTimeExtensions.cs
+++ DotNetDateUtils/DateTimeExtensions.cs
@@ -4,6 +4,13 @@ namespace DotNetDateUtils
 {
 	public static class DateTimeExtensions
 	{
+		/// <summary>
+		/// Calculates the integer number of days until the next future occurrence of a specific day of the week.
+		/// A partial day is counted as one - e.g., If today is Monday 1pm, DaysUntil Tuesday would return 1, not 11/24
+		/// </summary>
+		/// <param name="date"></param>
+		/// <param name="day"></param>
+		/// <returns></returns>
 		public static int DaysUntil(this DateTime date, DayOfWeek day)
 		{
 			int daysUntil = 0;
@@ -23,7 +30,7 @@ namespace DotNetDateUtils
 		}
 
 		/// <summary>
-		/// Finds the date (without time) of the next future occurrence of a specific date of the week
+		/// Finds the date (without time) of the next future occurrence of a specific day of the week
 		/// </summary>
 		/// <param name="date"></param>
 		/// <param name="day"></param>
@@ -33,6 +40,16 @@ namespace DotNetDateUtils
 			return date.AddDays(DaysUntil(date, day)).Date;
 		}
 
+		/// <summary>
+		/// Calculates the timespan until the next future occurrence of a specific day of the week
+		/// </summary>
+		/// <param name="date"></param>
+		/// <param name="day"></param>
+		/// <param name="hour"></param>
+		/// <param name="minute"></param>
+		/// <param name="second"></param>
+		/// <param name="millisecond"></param>
+		/// <returns></returns>
 		public static TimeSpan TimeUntil(this DateTime date, DayOfWeek day, int hour, int minute = 0, int second = 0, int millisecond = 0)
 		{
 			DateTime targetDateSameTime = date.AddDays(DaysUntil(date, day));
